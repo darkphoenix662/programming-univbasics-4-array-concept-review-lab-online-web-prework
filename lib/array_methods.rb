b92c2f114701159ae252d76b1fccaabc9483adc9
Sean Good <tree662@protonmail.com>
2019-10-26 04:35:50 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
-  scale.index(2)
-  scale.index(4)
-  scale_2.index(5)
-  scale_2.index(6)
+  scale.find_element_index(2)
+  scale.find_element_index(4)
+  scale_2.find_element_index(5)
+  scale_2.find_element_index(6)
 end
 
 def find_max_value(array)
