ea3117a770f2d8135a064a6233a400307534a86d
Sean Good <tree662@protonmail.com>
2019-10-26 05:35:47 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,6 +2,9 @@ def find_element_index(array, value_to_find)
   count = 0 
   while count < array.length do
     if array[count] == value_to_find
+      return count 
+    end
+    nil
 end
 
 def find_max_value(array)
